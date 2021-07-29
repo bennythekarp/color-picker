cb02eb5fe214a5ebdc7e39223f4855be5688bf60
BENJAMIN KARP <benjaminkarp@BENJAMINs-MBP.fios-router.home>
2021-07-29 00:17:30 -0400

    created colors app

---
+++ src/Palette.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+const Palette = ({ colors }) => {
+   console.log(colors)
+      return (
+         <div>
+            <h1>Palette</h1>
+            {}
+         </div>
+      )
+}
+
+export default Palette
